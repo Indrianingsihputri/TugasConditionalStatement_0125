4182112fff8ff362ab7dbfefffb178a4d8f646d2
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-03-31 21:27:34 +0700

    Menambahkan fungsi

--- BMI.cpp
+++ BMI.cpp
@@ -9,39 +9,8 @@ void inputData(float &berat, float &tinggi) {
     cin >> tinggi;
 }
 
+
 // Fungsi untuk menghitung BMI
 float hitungBMI(float berat, float tinggi) {
     return berat / (tinggi * tinggi);
-}
-
-// Fungsi untuk menentukan status BMI
-string statusBMI(float bmi) {
-    if (bmi < 18.5) {
-        return "Berat Badan Kurang";
-    } else if (bmi < 25) {
-        return "Berat Badan Normal";
-    } else if (bmi < 30) {
-        return "Berat Badan Kelebihan";
-    } else {
-        return "Obesitas";
-    }
-}
-
-int main() {
-    float berat, tinggi, bmi;
-
-    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
-
-    // Panggil prosedur input
-    inputData(berat, tinggi);
-
-    // Hitung BMI
-    bmi = hitungBMI(berat, tinggi);
-
-    // Output hasil
-    cout << "\n--- Hasil ---" << endl;
-    cout << "BMI Anda : " << bmi << endl;
-    cout << "Status   : " << statusBMI(bmi) << endl;
-
-    return 0;
 }
